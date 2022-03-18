1939d188dad6cc8c84fe11a624e7704ca29c9225
DORA3301 <101839244+DORA3301@users.noreply.github.com>
2022-03-18 15:21:18 +0100

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,9 +1,21 @@
 # Pong in Python
 
-
 import turtle
+import flask
 import os
- 
+from flask import send_from_directory
+
+app = flask.Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
+
+@app.route('/')
+@app.route('/home')
+
+
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
